e1f100848d7b00c12f711eb703fb80ec33bf5a4d
ShubhankarLondhe <50356124+ShubhankarLondhe@users.noreply.github.com>
2025-07-11 20:37:12 +0000

    Deploying to gh-pages from @ ShubhankarLondhe/shubhankarlondhe.github.io@97333edb79e37cee79f0ffca6d5acbc0f0ad3b9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-projects",
+          title: "projects",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
